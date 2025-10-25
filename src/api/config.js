c8ade98930f26d3faa313b894fb578637d841ac4
Peter Sekyere <psekyere@Peters-MacBook-Pro.local>
2025-10-25 06:04:24 +0000

    Update API_BASE_URL to Railway backend

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,4 @@
 // src/api/config.js
 
-export const API_BASE_URL = "https://futuresportalbackend-production.up.railway.app";
+const API_BASE_URL = "https://futuresportalbackend-production.up.railway.app";
+export default API_BASE_URL;
